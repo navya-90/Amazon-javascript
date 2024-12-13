b59456215df061ad372d23af28c58e45f0844692
navya <navyayadavny90@gmail.com>
2024-12-13 18:17:43 +0530

    Convert loadProducts from xhr to fetch

--- data/products.js
+++ data/products.js
@@ -54,6 +54,25 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+  const promise = fetch('https://supersimplebackend.dev/products').then((response) => {
+    return response.json();
+  }).then((productsData) => {
+    products = productsData.map((productDetails) =>{
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }else{
+        return new Product(productDetails);
+      }
+    });
+  });
+
+  return promise;
+}
+loadProductsFetch().then(() => {
+  console.log("next step");
+});
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
@@ -66,18 +85,16 @@ export function loadProducts(fun) {
       }
     });
 
-
     fun();
   });
   
-
   xhr.open('GET', 'https://supersimplebackend.dev/products');  
   xhr.send();
 }
 
-loadProducts(() => {
-  console.log('load product');
-});
+// loadProducts(() => {
+//   console.log('load product');
+// });
 
 
 // export const products = [
